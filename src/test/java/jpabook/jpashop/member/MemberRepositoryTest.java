f351e2a1b892a4b2516535ba30f68f17bd92d2fb
psbin2017 <123qkrtnqls@gmail.com>
2021-03-19 12:20:59 +0900

    [MOD] 엔티티 컬럼명 변경으로 반영

--- src/test/java/jpabook/jpashop/member/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/member/MemberRepositoryTest.java
@@ -44,7 +44,7 @@ public class MemberRepositoryTest {
 
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
         Assertions.assertThat(findMember).isEqualTo(member);
     }
 
